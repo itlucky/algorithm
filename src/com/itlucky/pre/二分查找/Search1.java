d331bb90c59fd8dc70f494b20697b61a0f984986
itlucky <hongjie_it@163.com>
2023-11-08 00:10:15 +0800

    #20231108_二分查找法比较

--- src/com/itlucky/pre/二分查找/Search1.java
+++ src/com/itlucky/pre/二分查找/Search1.java
@@ -3,7 +3,97 @@ package com.itlucky.pre.二分查找;
 /**
  * @Author itLucky
  * @Date 2023/11/7 22:04
- * @Description: TODO
+ * @Description: 查找某个数字在数组中的位置（索引下标），已知数组是有序的
  */
 public class Search1 {
+
+    public static void main(String[] args) {
+
+        int[] digital = {1, 3, 5, 6, 7, 9, 10, 14, 16, 23, 29, 33, 38};
+        int target = 99;
+
+        System.out.println(findIndex(digital, target));
+
+        System.out.println(findIndex_2(digital, target));
+
+        System.out.println(findIndex_3(digital, target));
+
+    }
+
+    /**
+     * 二分查找
+     * <p>
+     * i和j代表的索引范围是：[i,j]
+     *
+     * @param args
+     * @param target
+     * @return
+     */
+    private static int findIndex(int[] args, int target) {
+
+        int i = 0;
+        int j = args.length - 1;
+
+        while (i <= j) {
+            // 这里位运算，相当于(i+j)/2，使用>>> 可以避免数组长度为int最大值时出现的问题。
+            int m = (i + j) >>> 1;
+            if (target < args[m]) {
+                j = m - 1;
+            } else if (args[m] < target) {
+                i = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 二分查找-升级版
+     * <p>
+     * 时间复杂度是O(log_2(n))，比findIndex时间复杂度优。
+     * <p>
+     * 注意与上面写法的区分，i和j代表的索引范围是：[i,j），此时的j代表的是不包含要查找的范围
+     *
+     * @param args
+     * @param target
+     * @return
+     */
+    private static int findIndex_2(int[] args, int target) {
+        int i = 0;
+        int j = args.length;
+
+        while (i < j) {
+            int m = (i + j) >>> 1;
+            if (target < args[m]) {
+                j = m;
+            } else if (args[m] < target) {
+                i = m + 1;
+            } else {
+                return m;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 线性查询，时间复杂度为O(n),相对上面两种是最差的
+     * <p>
+     * 虽然代码看起来简单，但是算法不是最优
+     *
+     * @param args
+     * @param target
+     * @return
+     */
+    private static int findIndex_3(int[] args, int target) {
+        for (int i = 0; i < args.length; i++) {
+            if (target == args[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
